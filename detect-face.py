33c48bb4f766f42f3fbe292949d6f02ea022eca7
jojo4x4 <jonathan.balraj@gmail.com>
2020-01-25 15:15:39 -0600

    init

---
+++ detect-face.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# set to your own subscription key value
+subscription_key = os.getenv('KEY')
+assert subscription_key
+
+# replace <My Endpoint String> with the string from your endpoint URL
+face_api_url = os.getenv('API_URL')
+image_url = os.getenv('IMAGE_URL')
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion', #'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+}
+
+response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+
+def get_emotions(faces):
+    return [face['faceAttributes']['emotion'] for face in faces]
+
+response = response.json()
+print(get_emotions(response))
---
+++ quickstart-file.py
@@ -0,0 +1,41 @@
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+import requests
+from urllib.parse import urlparse
+from io import BytesIO
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
+
+# Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
+# This key will serve all examples in this document.
+KEY = '7f8b21a0a50b4be99dc9079287cf5679'
+
+# Set the FACE_ENDPOINT environment variable with the endpoint from your Face service in Azure.
+# This endpoint will be used in all examples in this quickstart.
+ENDPOINT = 'https://westcentralus.api.cognitive.microsoft.com/' #'https://garrett.cognitiveservices.azure.com/'
+
+# Create an authenticated FaceClient.
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+# Detect a face in an image that contains a single face
+single_face_image_url = 'https://www.biography.com/.image/t_share/MTQ1MzAyNzYzOTgxNTE0NTEz/john-f-kennedy---mini-biography.jpg'
+single_image_name = os.path.basename(single_face_image_url)
+detected_faces = face_client.face.detect_with_url(url=single_face_image_url)
+if not detected_faces:
+    raise Exception('No face detected from image {}'.format(single_image_name))
+
+# Display the detected face ID in the first single-face image.
+# Face IDs are used for comparison to faces (their IDs) detected in other images.
+print('Detected face ID from', single_image_name, ':')
+for face in detected_faces: print (face.face_id)
+print()
+
+# Save this ID for use in Find Similar
+first_image_face_ID = detected_faces[0].face_id
